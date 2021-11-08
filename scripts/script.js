dbc115540e2a063963748e22d83fcf7070144f33
Алексей Решетников <284503@itip.loc>
2021-11-08 15:26:37 +0300

    refactored 4th lab

--- scripts/script.js
+++ scripts/script.js
@@ -7,16 +7,8 @@ window.onload = function() {
    var element = document.createElement("div").appendChild(document.createElement('p').appendChild(document.createTextNode("Load time: " + loadTime + " sec")))
    footer[0].appendChild(element)
 
-   if (window.location.href.endsWith("works.html") == true) {
-      var curelem = document.getElementById("works")
-   }
-   else if (window.location.href.endsWith("index.html") == true
-         || window.location.href.endsWith("web/") == true) {
-      var curelem = document.getElementById("home")
-   }
-   else if (window.location.href.endsWith("achievements.html") == true) {
-      var curelem = document.getElementById("achievements")
-   }
-
-   curelem.style.borderBottom = "2px solid #d31c3e"
+   var curelem = document.querySelectorAll("[data-act]");
+   curelem.forEach(function(userItem) {
+      userItem.style.borderBottom = "2px solid #d31c3e";
+   })
 }
