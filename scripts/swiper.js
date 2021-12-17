11ed4ece290c3d266951037a13d01834222e32c3
Aleksey <ziqunion95@gmail.com>
2021-12-17 21:32:26 +0300

    make lab7

---
+++ scripts/swiper.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', function() {
+   // 1-ый аргумент - ".mySwiper" - класс, внутри элемента которого будет свайпер
+   // 2-ой аргемунт - объект с настройками свайпера
+   // pagination - пагинация
+   // pagination.el - класс элемента, храниящий пагинацию
+   // pagination.clickable - добавляет кликабельность пагинации
+   // navigation - навигация (кнопки влево-вправо)
+   // navigation.nextEl - класс элемента, хранящий кнопку вправо
+   // navigation.prevEl - класс элемента, храняйщий кнопку влево
+   // loop - цикличность навигации
+   var swiper = new Swiper(".mySwiper", {
+     pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+     },
+
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     },
+
+     loop: true,
+    });
+});
