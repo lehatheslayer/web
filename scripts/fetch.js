4d2ec099bde416d51712a4484f36bb01808fd857
Aleksey <ziqunion95@gmail.com>
2021-12-17 19:24:29 +0300

    edit lab 6

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -4,41 +4,43 @@ window.addEventListener('DOMContentLoaded', function() {
 
    let gallery_container = document.createElement("div");
    gallery_container.classList.add("gallery");
+   let links = ['https://jsonplaceholder.typicode.com/photos?albumId=1', 'https://jsonplaceholder.typicode.com/photos?albumId=2'];
 
-   fetch('https://jsonplaceholder.typicode.com/photos')
-      .then((response) => {
-         if (!response.ok) {
-            throw new Error(response.status);
-         } else {
-            return response.json();
-         }
-      })
-      .then((json) => {
-         document.getElementById('result').innerHTML = '';
+   links.forEach((link) => {
+      fetch(link)
+         .then((response) => {
+            if (!response.ok) {
+               throw new Error(response.status);
+            } else {
+               return response.json();
+            }
+         })
+         .then((json) => {
+            document.getElementById('result').innerHTML = '';
 
-         if (localStorage.getItem("number") == 1){
-           localStorage.setItem("number", 0);
-         }
-         else {
-              localStorage.setItem("number", 1);
-         }
+            if (localStorage.getItem("number") == 1){
+              localStorage.setItem("number", 0);
+            }
+            else {
+                 localStorage.setItem("number", 1);
+            }
 
-         json.filter(FilterFunc).forEach((item) => {
-            console.log(item)
-            var image = document.createElement("img");
-            image.src = item.url;
-            image.classList.add("gallery_item");
-            image.style.width = "300px";
-            image.style.height = "300px";
+            json.filter(FilterFunc).forEach((item) => {
+               var image = document.createElement("img");
+               image.src = item.url;
+               image.classList.add("gallery_item");
+               image.style.width = "300px";
+               image.style.height = "300px";
 
-            gallery_container.appendChild(image);
+               gallery_container.appendChild(image);
+            });
+
+            document.getElementById('result').appendChild(gallery_container);
+         })
+         .catch((error) => {
+            document.getElementById('result').innerHTML = '<div class="cssload-container"><p>⚠Что-то пошло не так</p></div>';
+            console.log('Error: ' + error);
          });
-
-         document.getElementById('result').appendChild(gallery_container);
-      })
-      .catch((error) => {
-         document.getElementById('result').innerHTML = '<div class="cssload-container"><p>⚠Что-то пошло не так</p></div>';
-         console.log('Error: ' + error);
       });
 });
 
